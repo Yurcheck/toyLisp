c188cacbeff552355e6128fa1b13a91aaf3973b3
Yurcheck <101168432+Yurcheck@users.noreply.github.com>
2024-04-07 08:28:12 +0300

    Main functions init

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+use std::error::Error;
+
+pub const LANG_NAME: &'static str = "ToyLISP";
+
+pub fn run_file(file_path: &str) -> Result<(), Box<dyn Error>> {
+    println!("{}", file_path);
+
+    Ok(())
+}
+
+pub fn run_prompt() -> Result<(), Box<dyn Error>> {
+    todo!();
+}
+
+pub fn run(source: String) -> Result<(), Box<dyn Error>> {
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::{env, process};
+
+use toy_lisp::{run_file, run_prompt, LANG_NAME};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 1 {
+        if let Err(err) = run_prompt() {
+            println!("[{}] {}", LANG_NAME, err);
+            process::exit(1);
+        }
+    }
+    else if args.len() == 2 {
+        let file_path = &args[1];
+
+        if let Err(err) = run_file(file_path) {
+            println!("[{}] {}", LANG_NAME, err);
+            process::exit(1);
+        }
+    }
+    else {
+        println!("[{}] Too many arguments given", LANG_NAME);
+        process::exit(1);
+    }
 }
